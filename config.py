7f86c97da9ded560a6a423c51917cd715160e0a3
Un-bots <161623523+Un-bots@users.noreply.github.com>
2024-03-08 18:23:00 +0530

    Update config.py

--- config.py
+++ config.py
@@ -53,7 +53,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 # ----------------------------------------------------------------
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://t.me/harsh_un",
+    "https://github.com/un-bots/UN-MUSIC",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "Master")
 GIT_TOKEN = getenv(
