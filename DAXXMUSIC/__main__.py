2f275ee0c94415acd8f3878af053967a67221204
Un-bots <161623523+Un-bots@users.noreply.github.com>
2024-03-10 10:37:01 +0530

    Update __main__.py

--- DAXXMUSIC/__main__.py
+++ DAXXMUSIC/__main__.py
@@ -42,7 +42,7 @@ async def init():
     try:
         await DAXX.stream_call("https://te.legra.ph/file/29f784eb49d230ab62e9e.mp4")
     except NoActiveGroupCall:
-        LOGGER("DAXXMUSIC").error(
+        LOGGER("SHADOW").error(
             "𝗣𝗹𝗭 𝗦𝗧𝗔𝗥𝗧 𝗬𝗢𝗨𝗥 𝗟𝗢𝗚 𝗚𝗥𝗢𝗨𝗣 𝗩𝗢𝗜𝗖𝗘𝗖𝗛𝗔𝗧\𝗖𝗛𝗔𝗡𝗡𝗘𝗟\n\nSʜᴀᴅᴏᴡ Cʀᴏɴᴜꜱ 𝗦𝗧𝗢𝗣........"
         )
         exit()
