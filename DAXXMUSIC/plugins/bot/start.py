ac7389920c3276371e08db5210250db845c77f96
Un-bots <161623523+Un-bots@users.noreply.github.com>
2024-03-10 14:22:49 +0530

    Update start.py

--- DAXXMUSIC/plugins/bot/start.py
+++ DAXXMUSIC/plugins/bot/start.py
@@ -30,10 +30,10 @@ from strings import get_string
 
 NEXI_VID = [
 "https://telegra.ph/file/1a3c152717eb9d2e94dc2.mp4",
-"https://graph.org/file/ba7699c28dab379b518ca.mp4",
+"https://graph.org/file/412992cc6183fa30558a7.mp4",
 "https://graph.org/file/83ebf52e8bbf138620de7.mp4",
 "https://graph.org/file/82fd67aa56eb1b299e08d.mp4",
-"https://graph.org/file/318eac81e3d4667edcb77.mp4",
+"https://graph.org/file/db3e0d203162c4de45ec7.mp4",
 "https://graph.org/file/7c1aa59649fbf3ab422da.mp4",
 "https://graph.org/file/2a7f857f31b32766ac6fc.mp4",
 
