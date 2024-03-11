9a1fe4053ba3726acf292cde15a15a3006b0b8f7
Un-bots <161623523+Un-bots@users.noreply.github.com>
2024-03-11 12:46:12 +0530

    Update couples.py

--- DAXXMUSIC/plugins/tools/couples.py
+++ DAXXMUSIC/plugins/tools/couples.py
@@ -14,8 +14,8 @@ from DAXXMUSIC.mongo.couples_db import _get_image, get_couple
 POLICE = [
     [
         InlineKeyboardButton(
-            text="𓊈𒆜彡[𝐃𝚊𝚡𝚡 𝐂𝙲 𝐂𝙻𝚄𝙱 ]彡𒆜𓊉",
-            url=f"https://t.me/ALLTYPECC",
+            text="𓊈𒆜彡[¢нαт gяσυρ]彡𒆜𓊉",
+            url=f"https://t.me/LU639R",
         ),
     ],
 ]
@@ -108,7 +108,7 @@ async def ctest(_, message):
          TXT = f"""
 **ᴛᴏᴅᴀʏ's ᴄᴏᴜᴘʟᴇ ᴏғ ᴛʜᴇ ᴅᴀʏ :
 
-{N1} + {N2} = 💚
+{N1} + {N2} = 💝
 
 ɴᴇxᴛ ᴄᴏᴜᴘʟᴇs ᴡɪʟʟ ʙᴇ sᴇʟᴇᴄᴛᴇᴅ ᴏɴ {tomorrow} !!**
 """
