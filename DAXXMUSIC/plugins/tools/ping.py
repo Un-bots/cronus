12f499262919a91498159b5e1fba492a174c448b
Un-bots <161623523+Un-bots@users.noreply.github.com>
2024-03-13 21:38:51 +0530

    Update ping.py

--- DAXXMUSIC/plugins/tools/ping.py
+++ DAXXMUSIC/plugins/tools/ping.py
@@ -24,5 +24,5 @@ async def ping_com(client, message: Message, _):
     UP, CPU, RAM, DISK = await bot_sys_stats()
     resp = (datetime.now() - start).microseconds / 1000
     await response.edit_text(
-        _["ping_2"].format(resp, app.mention, UP, RAM, CPU, DISK, pytgping)
-    )
+        _["ping_2"].format(resp, app.mention, UP, RAM, CPU, DISK, pytgping),
+   By >> HARSH🗿 )
