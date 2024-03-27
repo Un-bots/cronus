46a86571eeeeab1658a0dfc4daff0493538fa32e
Un-bots <161623523+Un-bots@users.noreply.github.com>
2024-03-27 14:11:05 +0530

    Update couples.py

--- DAXXMUSIC/plugins/tools/couples.py
+++ DAXXMUSIC/plugins/tools/couples.py
@@ -10,6 +10,7 @@ from pyrogram.enums import *
 #BOT FILE NAME
 from DAXXMUSIC import app as app
 from DAXXMUSIC.mongo.couples_db import _get_image, get_couple
+from config.py import BOT_USERNAME 
 
 POLICE = [
     [
