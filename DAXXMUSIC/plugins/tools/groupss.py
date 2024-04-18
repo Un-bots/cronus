7f9ae70f5d53c7ac0d84884d8e80e782f32dfe7d
Un-bots <161623523+Un-bots@users.noreply.github.com>
2024-04-18 09:48:13 +0530

    Update groupss.py

--- DAXXMUSIC/plugins/tools/groupss.py
+++ DAXXMUSIC/plugins/tools/groupss.py
@@ -5,13 +5,13 @@ import html
 from telegram import Update
 from telegram.ext import CommandHandler, CallbackContext
 
-
+from config import OWNER_ID
 
 
 
 async def send_groups_document(update: Update, context: CallbackContext) -> None:
-    if str(update.effective_user.id) not in SUDO_USERS:
-        update.message.reply_text('Only For Sudo users...')
+    if str(update.effective_user.id) not in OWNER_ID:
+        update.message.reply_text('Only For owner...')
         return
     cursor = top_global_groups_collection.find({})
     groups = []
