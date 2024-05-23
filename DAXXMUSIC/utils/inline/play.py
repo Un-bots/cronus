5e39fa609da49736f8289f5a204cfdc2cd54eca7
Un-bots <161623523+Un-bots@users.noreply.github.com>
2024-05-23 15:21:49 +0530

    Update play.py

--- DAXXMUSIC/utils/inline/play.py
+++ DAXXMUSIC/utils/inline/play.py
@@ -60,10 +60,13 @@ def stream_markup_timer(_, chat_id, played, dur):
             )
         ],
         [
-            InlineKeyboardButton(text="ʀᴇsᴜᴍᴇ", callback_data=f"ADMIN Resume|{chat_id}"),
-            InlineKeyboardButton(text="ᴘᴀᴜsᴇ", callback_data=f"ADMIN Pause|{chat_id}"),
             InlineKeyboardButton(text="sᴋɪᴘ", callback_data=f"ADMIN Skip|{chat_id}"),
             InlineKeyboardButton(text="sᴛᴏᴘ", callback_data=f"ADMIN Stop|{chat_id}"),
+            InlineKeyboardButton(text="ᴘᴀᴜsᴇ", callback_data=f"ADMIN Pause|{chat_id}"),
+        ],
+       [     
+           InlineKeyboardButton(text="ʀᴇsᴜᴍᴇ", callback_data=f"ADMIN Resume|{chat_id}"), 
+           InlineKeyboardButton(text="", callback_data=f"ADMIN Resume|{chat_id}"),
         ],
         
         [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
