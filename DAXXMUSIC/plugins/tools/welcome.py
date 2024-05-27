3864adf1c226bdb6fa0eeae6da2590d8f78ef11f
Un-bots <161623523+Un-bots@users.noreply.github.com>
2024-05-27 20:16:08 +0800

    Update welcome.py

--- DAXXMUSIC/plugins/tools/welcome.py
+++ DAXXMUSIC/plugins/tools/welcome.py
@@ -109,7 +109,7 @@ def welcomepic(pic, user, chatname, id, uname, brightness_factor=1.3):
     background = Image.open("DAXXMUSIC/assets/wel2.png")
     pfp = Image.open(pic).convert("RGBA")
     pfp = circle(pfp, brightness_factor=brightness_factor) 
-    pfp = pfp.resize((200, 175))
+    pfp = pfp.resize((175, 175))
     draw = ImageDraw.Draw(background)
     font = ImageFont.truetype('DAXXMUSIC/assets/font.ttf', size=70)
     welcome_font = ImageFont.truetype('DAXXMUSIC/assets/font.ttf', size=61)
