36d3c0795c77cb6673b09e28357f89dab6b3d9e2
Un-bots <161623523+Un-bots@users.noreply.github.com>
2024-08-22 17:15:37 +0530

    Update help.py

--- DAXXMUSIC/utils/inline/help.py
+++ DAXXMUSIC/utils/inline/help.py
@@ -94,6 +94,34 @@ def help_pannel(_, START: Union[bool, int] = None):
                     callback_data="help_callback hb15",
                 ),
             ],
+            [
+                InlineKeyboardButton(
+                    text=_["H_B_16"],
+                    callback_data="help_callback hb16",
+                ),
+                InlineKeyboardButton(
+                    text=_["H_B_17"],
+                    callback_data="help_callback hb17",
+                ),
+                InlineKeyboardButton(
+                    text=_["H_B_18"],
+                    callback_data="help_callback hb18",
+                ),
+            ],
+            [
+                InlineKeyboardButton(
+                    text=_["H_B_19"],
+                    callback_data="help_callback hb19",
+                ),
+                InlineKeyboardButton(
+                    text=_["H_B_20"],
+                    callback_data="help_callback hb20",
+                ),
+                InlineKeyboardButton(
+                    text=_["H_B_21"],
+                    callback_data="help_callback hb21",
+                ),
+            ],
             mark,
         ]
     )
