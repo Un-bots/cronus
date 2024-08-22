afe55cbfa8d6a12430f76a5ea4003b6b8caf0f2c
Un-bots <161623523+Un-bots@users.noreply.github.com>
2024-08-22 12:09:33 +0530

    Update gitinfo.py

--- DAXXMUSIC/plugins/tools/gitinfo.py
+++ DAXXMUSIC/plugins/tools/gitinfo.py
@@ -6,13 +6,6 @@ from DAXXMUSIC import app
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
 ###
-@app.on_message(filters.command("daxxhub"))
-async def daxxhub(_, message):
-    text = message.text[len("/daxxhub") :]
-    papadaxx(f"{text}").save(f"daxxhub_{message.from_user.id}.png")
-    await message.reply_photo(f"daxxhub_{message.from_user.id}.png")
-    os.remove(f"daxxhub_{message.from_user.id}.png")
-####
 
 @app.on_message(filters.command(["github", "git"]))
 async def github(_, message):
