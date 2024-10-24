0af7d30f3c96498ea1873b2680bc9a9f539a8526
Un-bots <161623523+Un-bots@users.noreply.github.com>
2024-10-24 19:43:02 +0530

    Create ig.py

---
+++ DAXXMUSIC/plugins/tools/ig.py
@@ -0,0 +1,53 @@
+from requests import get 
+from pyrogram import filters
+from pyrogram.types import InputMediaPhoto
+# Import your bot client from REPO
+from DAXXMUSIC import app as app
+import asyncio
+
+"
+
+
+@app.on_message(filters.command(["insta","igd"]))
+async def insta_download(client, message):
+    try:
+        url = message.text.split(None, 1)[1]
+    except IndexError:
+        return await message.reply("Provide URL. Usage: /insta [url]")
+
+    chat_id = message.chat.id
+
+    try:
+        response = get(f"https://hoshi-api-f62i.onrender.com/api/insta?url={url}")
+        if response.status_code != 200:
+            return await message.reply(f"Error fetching Instagram data. Status code: {response.status_code}\nReport @HoshinoXUpdates")
+
+        data = response.json()
+        media_list = data.get("result", [])
+
+        if url.startswith("https://www.instagram.com/p/"):
+            media_group = []
+            for media_url in media_list[:8]:
+                media_group.append(InputMediaPhoto(media=media_url))
+
+            lalal = await message.reply(f"=> Fetched {len(media_group)} Media")
+            await pbot.send_media_group(chat_id=chat_id, media=media_group)
+            await lalal.delete()
+
+        elif url.startswith("https://www.instagram.com/reel/"):
+            for video_url in media_list:
+                await pbot.send_video(chat_id=chat_id, video=video_url)
+
+        elif url.startswith("https://www.instagram.com/stories/"):
+            for story_url in media_list:
+                try:
+                    await pbot.send_video(chat_id=chat_id, video=story_url)
+                    await asyncio.sleep(0.3)
+                except Exception as e:
+                    await message.reply(f"Error sending video: {e}")
+
+        else:
+            await message.reply("Invalid Instagram link")
+
+    except Exception as e:
+        await message.reply(f"Error: {e}")
